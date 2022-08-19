b5722a373d26ad3dd28cbe6d834a22e95ef9dc9c
AZYxV <nschpro@gmail.com>
2022-08-19 14:18:05 +0200

    feat(commitlint): add and setup commitlint

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,9 +5,16 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+        
+    }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
@@ -19,7 +26,7 @@ class AppFixtures extends Fixture
             ->setAge(50)
             ->setUsername('stefbk')
             ->setEmail('stef@stef.com')
-            ->setPassword('0000')
+            ->setPassword($this->hasher->hashPassword($user, 'test1234'))
             ->setRoles(["ROLE_ADMIN"])
             ->setVille('VALENCE');
 
